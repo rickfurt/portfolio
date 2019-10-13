e9007d8aa919ef4da0602f311330799b8e014492
rickfurt <ricardofurtado2609@gmail.com>
2019-10-13 15:33:00 +1100

    chore(infra): create db schema,model

--- src/server.js
+++ src/server.js
@@ -15,6 +15,25 @@ db.once("open", function() {
   console.log("database connected");
 });
 
+var Schema = mongoose.Schema;
+
+var projectSchema = new mongoose.Schema({
+    title: String,
+    description:  String,
+    url: String
+  });
+
+var Project = mongoose.model('Project', projectSchema);
+
+function saveProjectToDb(title, description, url){
+    var newProject = new Project({title,description,url});
+    
+    newProject.save(function (err) {
+      if (err) return console.error(err);
+      console.log('Project Saved');
+    });
+  }
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Server running on port ${port}!`));
