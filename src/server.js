da1332a305fe875456789d3440a617e371870a27
rickfurt <ricardofurtado2609@gmail.com>
2019-10-13 14:44:38 +1100

    chore(infra): setup database connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
+require("dotenv").config();
+var mongoose = require("mongoose");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-app.listen(port, () => console.log(`Server running on port ${port}!`))
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("database connected");
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Server running on port ${port}!`));
