06a38e2cdd3fba8d92d93c7006c7ff2c1a04586f
rickfurt <ricardofurtado2609@gmail.com>
2019-10-31 19:12:22 +1100

    chore(infra): include components folder to prettier and eslint checking

--- components/index.js
+++ components/index.js
@@ -1,18 +1,14 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'
-
-
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() { 
-        return ( 
-            <h1>All the Components will be rendered here...</h1>
-         );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>All the Components will be rendered here...</h1>;
+  }
 }
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
