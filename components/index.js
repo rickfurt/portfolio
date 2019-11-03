9c4c72f858d7b36a56f514b69c78fbcb61f7d727
rickfurt <ricardofurtado2609@gmail.com>
2019-11-03 18:25:05 +1100

    chore(infra): create fetch method to get projects data

--- components/index.js
+++ components/index.js
@@ -2,20 +2,36 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Menubar from "./menubar/Menubar";
 import About from "./about/About";
-import Skills from './skills/Skills'
+import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
 
 import "./index.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      projects: []
+    };
   }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/projects")
+      .then(response => response.json())
+      .then(result => {
+        this.setState({
+          projects: result
+        });
+      });
+  }
+
   render() {
     return (
       <>
         <Menubar />
         <About />
         <Skills />
+        <Projects projects={this.state.projects} />
       </>
     );
   }
--- components/projects/Projects.js
+++ components/projects/Projects.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./projects.css";
 
 function Projects(props) {
+  console.log(props);
   return (
     <ul>
       {props.projects.map(data => (
-        <li key={data.ObjectID}>
+        <li key={data._id}>
           <a href={data.url}>{data.title}</a>
         </li>
       ))}
